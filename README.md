0d02b04e8428b056ed2e16a6841db54e6efed39d
Nate Solis <66754842+coldenate@users.noreply.github.com>
2023-12-08 13:52:09 +0000

    init repository

---
+++ README.md
@@ -0,0 +1,8 @@
+# advanced-remnote-plugin-template
+A slightly opinionated template for RemNote plugins designed to be on the particularly organized side of life. :)
+
+## Remember
+
+- [ ] Change `ts` files to `tsx` files where necessary
+- [ ] Change `manifest.json` to reflect your plugin
+- [ ] Edit the License to reflect your name
---
+++ src/widgets/index.ts
@@ -0,0 +1,39 @@
+import { declareIndexPlugin, ReactRNPlugin, RNPlugin } from '@remnote/plugin-sdk';
+
+async function onActivate(plugin: ReactRNPlugin) {
+	// settings
+
+	await plugin.settings.registerBooleanSetting({
+		id: 'debug-mode',
+		title: 'Debug Mode',
+		description: 'Enables certain testing commands. Non-destructive.',
+		defaultValue: false,
+	});
+
+	// commands
+
+	plugin.track(async (reactivePlugin) => {
+		await isDebugMode(reactivePlugin).then(async (debugMode) => {
+			if (debugMode) {
+				plugin.app.toast('Debug Mode Enabled; Registering Debug Tools');
+				await plugin.app.registerCommand({
+					id: 'log-values',
+					name: 'Log Values',
+					description: 'Log the values of certain variables',
+					quickCode: 'debug log',
+					action: async () => {
+						// log values
+					},
+				});
+			}
+		});
+	});
+
+	async function isDebugMode(reactivePlugin: RNPlugin): Promise<boolean> {
+		return await reactivePlugin.settings.getSetting('debug-mode');
+	}
+}
+
+async function onDeactivate(_: ReactRNPlugin) {}
+
+declareIndexPlugin(onActivate, onDeactivate);
